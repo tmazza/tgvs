07640b38879b415895228bab1891fd9ace8536c0
Tiago Mazzarollo de Paula <tiago.mazzarollo@ad.ufrgs.br>
2016-02-10 17:37:00 -0200

    add script google analitycs

--- themes/2016/views/layouts/main.php
+++ themes/2016/views/layouts/main.php
@@ -12,6 +12,19 @@
 		<link rel="stylesheet" href="<?=Yii::app()->baseUrl . '/themes/2016/assets/css/main.css';?>" />
 		<link rel="stylesheet" href="<?=Yii::app()->baseUrl . '/themes/2016/assets/css/olivi.css';?>" />
 		<script src="http://code.jquery.com/jquery-1.12.0.min.js" ></script>
+
+		<?php if(!YII_DEBUG): ?>
+			<script>
+			  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+			  ga('create', 'UA-73593676-1', 'auto');
+			  ga('send', 'pageview');
+			</script>
+		<?php endif; ?>
+
 	</head>
 	<body id="top">
 		
