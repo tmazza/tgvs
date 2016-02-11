d4a6a911d486fec2fe46b37f5bb20dcb646043a5
Oliver Hung Buo Tso <olivertsor@gmail.com>
2016-02-11 21:34:34 -0200

    search input

--- themes/2016/assets/js/olivi.js
+++ themes/2016/assets/js/olivi.js
@@ -1,17 +1,22 @@
 $(document).ready(function () {
   $searchArea = $('#search-area');
   $listArea = $('#list-area');
-  // $addedShows = $('.added-img');
   $areaSwitchBtn = $('#area-switch-btn');
 
   $listArea.hide();
+  $('#search-results').hide();
 
   $areaSwitchBtn.click(function () {
     $listArea.slideToggle();
     $searchArea.slideToggle();
   });
 
-  // $addedShows.click(function (event) {
-  //   $(event.target).parent().remove();
-  // });
+  $('#search-input').keyup(function () {
+    if ($('#search-results').is(':hidden')) {
+      $('#search-results').fadeIn();
+    }
+    else if ($('#search-input').val().length === 0) {
+      $('#search-results').hide();
+    }
+  });
 });
--- themes/2016/views/layouts/main.php
+++ themes/2016/views/layouts/main.php
@@ -9,66 +9,10 @@
 		<title>Tempo Gasto Vendo Séries</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+		<script src="http://code.jquery.com/jquery-1.12.0.min.js" ></script>
 		<link rel="stylesheet" href="<?=Yii::app()->baseUrl . '/themes/2016/assets/css/main.css';?>" />
 		<link rel="stylesheet" href="<?=Yii::app()->baseUrl . '/themes/2016/assets/css/olivi.css';?>" />
-		<script src="http://code.jquery.com/jquery-1.12.0.min.js" ></script>
-
-		<style type="text/css">
-			.div-image {
-				cursor: pointer;
-				display: inline-block;
-				vertical-align: top;
-				margin-left: 0.5em;
-		    	border: 2px solid transparent;	
-			}
-			.div-image img {
-			    opacity: 1;
-			    transition: opacity 0.2s ease-in-out;
-			    -moz-transition: opacity 0.2s ease-in-out;
-			    -webkit-transition: opacity 0.2s ease-in-out;
-			    -ms-transition: opacity 0.2s ease-in-out;
-			    transition: opacity 0.2s ease-in-out;
-
-			    width: 215px;
-			    border-radius: 0.35em;
-			}
-		    .div-image img:hover {
-		    	opacity: 0.3;
-		    }
-		    .div-image img.added {
-		    	opacity: 0.5;
-		    	/*border: 2px solid #F7C873;	*/
-		    	border: 2px solid #FFAB00;	
-		    }
-
-		    .div-image p {
-		    	width: 215px;
-		    	margin-bottom: 1em;
-		    	margin-top: -0.5em;
-		    	line-height: 18px;
-		    }
-		    #tempo div {
-		    	font-family: arial;
-		    	color: #ddd;
-		    	font-size: 60px;
-		    	display: inline-block;
-		    	vertical-align: top;
-		    	/*padding: 0px 5px;*/
-		    	margin: 0px 4px;
-		    }
-		    #tempo small{
-		    	color: #888;
-		    	font-size: 12px;
-		    }
-			.added-img > img {
-		    	width: 65px!important;
-		    	height: auto!important; /*Retirar do CSS antigo e depos tirar essa linha*/
-			}
-		    #added-gallery > a {
-				margin: -0.4em 0.1em 0.1em 0.1em;
-			}
-
-		</style>
 
 		<?php if(!YII_DEBUG): ?>
 			<script>
@@ -87,13 +31,8 @@
 		
 		<!-- Header -->
 		<header id="header">
-<!-- 			<div id="timer" style="text-align:center;">
-				<strong id='tempo'>00:00:00</strong>
-			</div> -->
-			<div id='tempo' style="text-align:center; padding: 40px 0px; font-size:36px;">
-				00:00:00
-			</div>
 
+			<div id='tempo'>00:00:00</div>
 			<div id="added-gallery"></div>
 
 		</header>
@@ -103,13 +42,14 @@
 
 			<!-- Two -->
 			<section id="two">
+
 				<ul class="actions" style="text-align:center;">
 					<li><a id="area-switch-btn" href="#!" class="button">Buscar / Listar</a></li>
 				</ul>
 
 				<div id="search-area">
 					<form>
-						<input id="search-input" type="text" placeholder="Digite o nome de um seriado" autocomplete="off">
+						<input id="search-input" type="text" placeholder="Digite o nome do seriado." autocomplete="off">
 					</form>
 					<div id='search-results'></div>
 				</div>
