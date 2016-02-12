f1c0d0f77a901fcf0e1fa2cd6bc2282fc45eda78
tmazza <tiagomdepaula@gmail.com>
2016-02-12 14:45:21 -0200

    add descricao e palvras-chaves

--- themes/2016/views/layouts/main.php
+++ themes/2016/views/layouts/main.php
@@ -9,8 +9,9 @@
 		<title>Tempo Gasto Vendo Séries</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
-
 		<script src="http://code.jquery.com/jquery-1.12.0.min.js" ></script>
+		<meta name="description" content="Calcule seu tempo total gasto assistindo seriados e animes." />
+		<meta name="keywords" content="calcular,serie,seriados,animes,tempo,gasto,perdido,assitindo,vendo,wasted,time,watching,tv show" />
 		<link rel="stylesheet" href="<?=Yii::app()->baseUrl . '/themes/2016/assets/css/main.css';?>" />
 		<link rel="stylesheet" href="<?=Yii::app()->baseUrl . '/themes/2016/assets/css/olivi.css';?>" />
 
